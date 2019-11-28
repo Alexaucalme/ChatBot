1f985d74a43f41b3756e5081405c330b7eff71b0
Alexaucalme <alexandre.monroche@pm.me>
2019-11-28 08:44:19 +0100

    Update

--- ChatBot.py
+++ ChatBot.py
@@ -6,3 +6,13 @@ Created on Thu Nov  7 18:32:14 2019
 @author: alexaucalme
 """
 
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+Dialogues = open("Sources/movie_lines_traduit.txt", "r", encoding = "utf-8", errors="ignore")
+TxtDialogue = Dialogues.read()
+
+Conversations = open("Sources/movie_conversations.txt", "r", encoding = "utf-8", errors="ignore")
+TxtConversations = Conversations.read()
