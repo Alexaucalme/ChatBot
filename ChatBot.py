d05f7685656623576a1c50e54cbdab37975be501
Alexaucalme <alexandre.monroche@pm.me>
2019-12-03 15:15:58 +0100

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -20,11 +20,11 @@ FichierConversations = open('Sources/movie_conversations.txt', 'r', encoding = '
 
 # Créer un dictionnaire pour associer chaque phrase à son ID
 
-DictionnairePhrases = {}                                                        # On créé notre dictionnaire
+DictionnairePhrasesVersID = {}                                                  # On créé notre dictionnaire
 for Ligne in FichierDialogues:                                                  # On parcours les dialogues
     LigneTemporaire = Ligne.split(' +++$+++ ')                                  # On sépare chaque ligne en tableau
     if len(LigneTemporaire) == 5:                                               # Si la longueur de la ligne est conforme
-        DictionnairePhrases[LigneTemporaire[0]] = LigneTemporaire[-1]           # On ajoute au dictionnaire la phrase avec le numéro du phrase en indice
+        DictionnairePhrasesVersID[LigneTemporaire[0]] = LigneTemporaire[-1]     # On ajoute au dictionnaire la phrase avec le numéro du phrase en indice
 
 # Créer une liste contenant toutes les conversations
 
@@ -45,9 +45,9 @@ Questions = []                                                                  
 Reponses = []                                                                   # On créé le tableau contenant les réponses
 for Conversation in Conversations:                                              # On parcours le tableau contenant les conversations
     for IndicePhrase in range(len(Conversation) - 1):                           # On parcours le tableau contenant les indices des phrases pour chaques conversations
-        if Conversation[IndicePhrase] in DictionnairePhrases.keys():            # Si l'indice a bien une case dans le dictionnaire
-            Questions.append(DictionnairePhrases[Conversation[IndicePhrase]])   # On ajoute le premier indice comme questions
-            Reponses.append(DictionnairePhrases[Conversation[IndicePhrase + 1]])# et le suivant comme réponse, ainsi une question peux être une réponse
+        if Conversation[IndicePhrase] in DictionnairePhrasesVersID.keys():      # Si l'indice a bien une case dans le dictionnaire
+            Questions.append(DictionnairePhrasesVersID[Conversation[IndicePhrase]])     # On ajoute le premier indice comme questions
+            Reponses.append(DictionnairePhrasesVersID[Conversation[IndicePhrase + 1]])  # et le suivant comme réponse, ainsi une question peux être une réponse
 
 # Nettoyage des questions et des réponses en enlevent [] () {} _ + =
 
@@ -69,20 +69,67 @@ for Reponse in Reponses:                                                        
 
 # Créé un dictionnaire qui associe à chaque mot le nombre de fois où il apparait
 
-DictionnaireMots = {}                                                           # On créé le dictionnaire
+DictionnaireMotsVersOccurence = {}                                              # On créé le dictionnaire
 
 for Question in QuestionsNettoye:                                               # On parcours le tableau des questions nettoyées
     for Mot in Question.split():                                                # On parcours chaque mot de la question "spliter"
-        if Mot not in DictionnaireMots:                                         # Si le mot n'est pas dans le Dictionnaire
-            DictionnaireMots[Mot] = 1                                           # On le place dans le dictionnaire avec une itération
+        if Mot not in DictionnaireMotsVersOccurence:                            # Si le mot n'est pas dans le Dictionnaire
+            DictionnaireMotsVersOccurence[Mot] = 1                              # On le place dans le dictionnaire avec une itération
         else:                                                                   # Sinon
-            DictionnaireMots[Mot] += 1                                          # On ajoute une itération au mot trouvé
+            DictionnaireMotsVersOccurence[Mot] += 1                             # On ajoute une itération au mot trouvé
 
 for Reponse in ReponsesNettoye:                                                 # Idem pour les réponses
     for Mot in Reponse.split():
-        if Mot not in DictionnaireMots:
-            DictionnaireMots[Mot] = 1
+        if Mot not in DictionnaireMotsVersOccurence:
+            DictionnaireMotsVersOccurence[Mot] = 1
         else:
-            DictionnaireMots[Mot] += 1
+            DictionnaireMotsVersOccurence[Mot] += 1
 
-# 
+# Créé les dictionnaires qui associent chaque mot d'une question (et d'une réponse) à un identifiant unique et leurs inverses
+
+NombreOccurenceMinimal = 20                                                     # On fixe le nombre d'occurence minimal d'un mot à 20
+DictionnairesMotsQuestionsVersIdentifiant = {}                                  # On créé le dictionnaire qui associe chaque mots des questions à un identifiant unique
+DictionnairesIdentifiantVersMotsQuestions = {}                                  # On créé le dictionnaire inverse qui associe chaque identifiant à un mot
+
+i = 0                                                                           # On initialise un compteur à 0
+for Question in QuestionsNettoye:                                               # Pour chaque question du tableau des questions nettoyées
+    for Mot in Question.split():                                                # Pour chaque mots de la question
+        if DictionnaireMotsVersOccurence[Mot] >= NombreOccurenceMinimal:        # Si ce mot est apparue au moins 20 fois dans toutes les phrases
+            if Mot not in DictionnairesMotsQuestionsVersIdentifiant:            # Si le mot n'est pas dans le dictionnaire, on l'ajoute, sinon on fait rien
+                DictionnairesMotsQuestionsVersIdentifiant[Mot] = i              # On le place dans les tableaux
+                DictionnairesIdentifiantVersMotsQuestions[i] = Mot
+                i += 1                                                          # On incrémente i de 1
+
+DictionnairesMotsReponsesVersIdentifiant = {}                                   # Idem pour
+DictionnairesIdentifiantVersMotsReponses = {}
+
+i = 0
+for Reponse in ReponsesNettoye:
+    for Mot in Reponse.split():
+        if DictionnaireMotsVersOccurence[Mot] >= NombreOccurenceMinimal:
+            if Mot not in DictionnairesMotsReponsesVersIdentifiant:
+                DictionnairesMotsReponsesVersIdentifiant[Mot] = i
+                DictionnairesIdentifiantVersMotsReponses[i] = Mot
+                i += 1
+
+# Enrichissement des dictionnaires avec les tokens
+
+Tokens = ['<EOS>', '<SOS>', '<PAD>', '<OUT>']                                   # Tableau contenant les tokens à rajouter
+for Token in Tokens:                                                            # Pour chaque tokens
+    DictionnairesMotsQuestionsVersIdentifiant[Token] = i                        # On l'ajoute à chaque tableau suivant leur nomenclature
+    DictionnairesIdentifiantVersMotsQuestions[i] = Token                        # En utilisant i étant à cette ligne l'indice du dernier mot
+    DictionnairesMotsReponsesVersIdentifiant[Token] = i
+    DictionnairesIdentifiantVersMotsReponses[i] = Token
+    i += 1                                                                      # On incrémente i de 1
+
+# Fonction pour ajouter un token à la fin des phrases d'un tableau
+
+def AjoutTokenFinPhrases(TableauPhrases, Token):                                # Définition de la fonction avec le tableau des phrases et le token à ajouter en paramètre
+    Token = ' ' + Token                                                         # On ajoute un espace avant le token
+    for i in range(len(TableauPhrases)):                                        # On parcours le tableau des phrases
+        TableauPhrases[i] += Token                                              # Pour chaque phrase on ajoute le Token à la fin
+    return TableauPhrases                                                       # On retourne le tableau modifier
+
+# Ajout du token <EOS> à la fin de chaque réponses
+
+QuestionsNettoye = AjoutTokenFinPhrases(QuestionsNettoye, Tokens[0])            # Appel de la fonction avec le Token <EOS> et les questions nettoyes en paramètres
